4e1873664e68788c3bc1cddd5a5c2f6c252214f3
mathewmoya6-star <mathewmoya6@gmail.com>
2026-05-09 15:33:23 +0300

    Update auth.routes.js

--- Backend/src/auth.routes.js
+++ Backend/src/auth.routes.js
@@ -2,26 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const authController = require("../controllers/auth.controller");
-const auth = require("../middleware/auth");
-const role = require("../middleware/role");
 
-// public
+// AUTH ROUTES
 router.post("/register", authController.register);
 router.post("/login", authController.login);
-
-// protected
-router.get("/profile", auth, (req, res) => {
-  res.json(req.user);
-});
-
-// admin only
-router.get("/admin", auth, role(["admin"]), (req, res) => {
-  res.json({ message: "Admin access granted" });
-});
-
-// driver only
-router.get("/driver", auth, role(["driver"]), (req, res) => {
-  res.json({ message: "Driver access granted" });
-});
 
 module.exports = router;
