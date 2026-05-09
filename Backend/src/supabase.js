b73e55fa42ee2b87d48886fb0fa3b35e93fb03d9
mathewmoya6-star <mathewmoya6@gmail.com>
2026-05-09 11:58:40 +0300

    Update supabase.js

--- Backend/src/supabase.js
+++ Backend/src/supabase.js
@@ -1,14 +1,8 @@
 const { createClient } = require("@supabase/supabase-js");
 
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY,
-  {
-    auth: {
-      autoRefreshToken: true,
-      persistSession: true,
-    },
-  }
-);
+const supabaseUrl = "https://fktjmkmzlixlapeyhhyl.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZrdGpta216bGl4bGFwZXloaHlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxNDkzNzEsImV4cCI6MjA5MjcyNTM3MX0.l0ZijiumKnwO7kcJYV8c7nDbvnfefBIwnMu4ieWCX2E";
+
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 module.exports = supabase;
