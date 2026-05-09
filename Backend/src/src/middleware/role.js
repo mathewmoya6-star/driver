94b1c454e7027d44fe87d70c0ce000f06e516798
mathewmoya6-star <mathewmoya6@gmail.com>
2026-05-09 14:39:56 +0300

    Rename parameter from allowedRoles to roles

--- Backend/src/src/middleware/role.js
+++ Backend/src/src/middleware/role.js
@@ -1,10 +1,10 @@
-module.exports = (allowedRoles) => {
+module.exports = (roles) => {
   return (req, res, next) => {
     if (!req.user) {
       return res.status(401).json({ message: "Unauthorized" });
     }
 
-    if (!allowedRoles.includes(req.user.role)) {
+    if (!roles.includes(req.user.role)) {
       return res.status(403).json({ message: "Access denied" });
     }
 
