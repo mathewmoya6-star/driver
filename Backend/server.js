18130d1faf21464f7a6816d72ef09e2198b66a31
mathewmoya6-star <mathewmoya6@gmail.com>
2026-05-10 22:52:59 +0300

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,38 +1,23 @@
 require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
 
 const app = express();
 
-/**
- * MIDDLEWARE
- */
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-/**
- * ROUTES
- */
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api", require("./routes/user.routes"));
+app.use("/api", require("./routes/admin.routes"));
 
-/**
- * HEALTH CHECK
- */
 app.get("/", (req, res) => {
-  res.json({ message: "MEI DRIVE AFRICA API RUNNING" });
+  res.json({ message: "API RUNNING" });
 });
 
-/**
- * START SERVER
- */
 const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
-  console.log("========================================");
-  console.log("SERVER IS RUNNING!");
-  console.log(`http://localhost:${PORT}`);
-  console.log("========================================");
+  console.log("SERVER RUNNING ON", PORT);
 });
