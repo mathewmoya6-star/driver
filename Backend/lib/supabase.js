ec5b57aed41a8f201bbdda713cefdfa5d0949170
mathewmoya6-star <mathewmoya6@gmail.com>
2026-05-10 22:54:10 +0300

    Update supabase.js

--- Backend/lib/supabase.js
+++ Backend/lib/supabase.js
@@ -1,7 +1,8 @@
 const { createClient } = require("@supabase/supabase-js");
 
-// IMPORTANT: NO realtime config at all
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_ANON_KEY
 );
+
+module.exports = supabase;
